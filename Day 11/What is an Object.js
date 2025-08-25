29fc4626b0c70e27bf335841056e29f1515b5f87
782-lab <mx08264@gmail.com>
2025-08-25 22:11:43 +0530

    What is obj

---
+++ Day 11/What is an Object.js
@@ -0,0 +1,14 @@
+/*An object is a collection of key-value pairs.
+
+Keys are called properties.
+
+Values can be anything: number, string, array, function, or another object. */
+
+const student = {
+  name: "Ashab",
+  age: 21,
+  isStudent: true
+};
+
+console.log(student.name);  // Output: Ashab
+console.log(student["age"]); // Output: 21
