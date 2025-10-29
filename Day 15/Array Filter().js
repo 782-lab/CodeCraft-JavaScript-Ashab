0ca84e802b24c1904512998b7782548ae55b7f00
Md Ashab Akhtar <153938074+782-lab@users.noreply.github.com>
2025-10-29 20:11:43 +0530

    Clean up console log output comment
    Removed duplicate comment line for clarity.

--- Day 15/Array Filter().js
+++ Day 15/Array Filter().js
@@ -1,3 +1,5 @@
 let numbers = [10, 15, 20, 25, 30];
 let evenNumbers = numbers.filter(num => num % 2 === 0);
-console.log(evenNumbers); // output  [10, 20, 30]
+console.log(evenNumbers); 
+
+// output  [10, 20, 30]
