c34e53b758235c77a1579c0238a534383666b533
Md Ashab Akhtar <153938074+782-lab@users.noreply.github.com>
2025-11-18 20:48:05 +0530

    Refactor comparison operator console logs

--- Day 13/Comparison Operators.js
+++ Day 13/Comparison Operators.js
@@ -1,9 +1,9 @@
 let a = 10;
 let b = "10";
 
-console.log(a == b);   // true (value only)
-console.log(a === b);  // false (value + type)
-console.log(a != b);   // false
-console.log(a !== b);  // true
-console.log(a > 5);    // true
-console.log(a <= 10);  // true
+console.log(a == b);   
+console.log(a === b);  
+console.log(a != b);   
+console.log(a !== b);  
+console.log(a > 5);    
+console.log(a <= 10);  
