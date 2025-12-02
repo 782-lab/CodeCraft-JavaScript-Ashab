bc6118bcdc5368687d068b96aabf7d3de4f3a988
782-lab <mx08264@gmail.com>
2025-12-03 03:43:50 +0530

    Palindrome Check (string)

---
+++ Day 89/prgm 3.js
@@ -0,0 +1,6 @@
+function isPalindrome(str) {
+  const s = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return s === s.split("").reverse().join("");
+}
+console.log("madam ->", isPalindrome("madam"));
+console.log("hello ->", isPalindrome("hello"));
