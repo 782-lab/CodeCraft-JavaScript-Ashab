f84c96c4577a0fb847e93a00d7fe76c93158c2ba
782-lab <mx08264@gmail.com>
2025-12-10 20:00:32 +0530

    Debounce (basic)

---
+++ Day 96/prgm 1.js
@@ -0,0 +1,9 @@
+function debounce(fn, delay){
+  let t;
+  return (...args)=>{
+    clearTimeout(t);
+    t=setTimeout(()=>fn(...args), delay);
+  };
+}
+// Example (uncomment to test in browser): window.addEventListener('resize', debounce(()=>console.log('resized'), 300));
+console.log("Debounce defined");
