c246661a2ad09f6f996df8ed4ab89dc28f311aea
Md Ashab Akhtar <153938074+782-lab@users.noreply.github.com>
2026-02-16 22:54:08 +0530

    Fix formatting of randomPassword function

--- Day 100/prgm 3.js
+++ Day 100/prgm 3.js
@@ -1,4 +1,5 @@
-function randomPassword(len=12){
+function randomPassword(len=12)
+{
   const chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
   let out=""; for(let i=0;i<len;i++) out += chars[Math.floor(Math.random()*chars.length)];
   return out;
